82452f5eae6208c2a35c60dfc80c7c034b7ad043
A60-0141 <endedenaville@gmail.com>
2025-02-12 00:08:19 +0300

    changed

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-f^u@bwr%wspr!&3btezd-3b=^jh80sqbtag1%!s@$fe62@d7tf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['mich-hybrid-auto-tech-app-production.up.railway.app']
+ALLOWED_HOSTS = ['mich-hybrid-auto-tech-app-production.up.railway.app', '*']
 
 CSRF_TRUSTED_ORIGINS = [
     "https://mich-hybrid-auto-tech-app-production.up.railway.app",
